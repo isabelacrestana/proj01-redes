031723e16682087400c07b54c0c1eaa657957319
Isabela Crestana <crestanaisabela@gmail.com>
2025-09-17 16:19:54 -0300

    Finalizando parte 2 do projeto e arrumando handle_cliente()

--- server.py
+++ server.py
@@ -2,13 +2,14 @@ import socket
 import threading
 import psutil
 import time
+import sys
 from datetime import datetime
 
 monitores_ativos = {}   #chave uma tupla(client_socket, monitor_id) -> {"tipo": "CPU"/"Memória", "ativo": True, "thread": thread, "intervalo": intervalo}. Um dicionário para armazenar os monitores ativos, sendo os dados desses monitores guardados em dicionarios também só que internos 
 lock = threading.Lock() #para garantir acesso exclusivo aos monitores ativos ao dicionário compartilhado entre as threads
 clientes_ativos = 0
 lock_clientes = threading.Lock()  #para garantir acesso exclusivo ao contador de clientes ativos
-max_clientes = 3  #número máximo de clientes simultâneos
+max_clientes = int(sys.argv[1])  #número máximo de clientes simultâneos
 
 def monitor_cpu(client_socket, intervalo, monitor_id):
     #primeira leitura descartada para não mostrar uso 0%
@@ -38,21 +39,28 @@ def handle_client(client_socket, addr):
     global clientes_ativos  #variável global para contar clientes ativos
     monitor_id = 0  #É o id de cada monitor para cada cliente.
     threads_monitor = []    #lista para armazenar as threads de monitoramento ativas para esse cliente
+    with lock_clientes:
+        clientes_ativos += 1
+        if clientes_ativos > max_clientes:
+            client_socket.sendall("Máximo de clientes atingido. Tente novamente mais tarde.\n".encode())
+            client_socket.close()
+            clientes_ativos -= 1
+            return #garante que não vai continuar tentando enviar
     #mandando mensagem ao conectar
     client_socket.sendall(f"{datetime.now().strftime('%H:%M:%S')}: CONECTADO!!\n".encode()) #sendall garante que toda a mensagem seja enviada e nao só parte dela como pode ocorrer com o send
     client_socket.sendall( "Comandos disponíveis:\n"
-                "cpu-<segundos>  → Inicia monitor de CPU\n"
-                "mem-<segundos>  → Inicia monitor de Memória\n"
-                "list  → Lista monitores ativos\n"
-                "quit-<ID>  → Encerra monitor pelo ID\n"
-                "exit  → Encerra todos os monitores ativos e sai\n".encode())
+        "cpu-<segundos>  → Inicia monitor de CPU\n"
+        "mem-<segundos>  → Inicia monitor de Memória\n"
+        "list  → Lista monitores ativos\n"
+        "quit-<ID>  → Encerra monitor pelo ID\n"
+        "exit  → Encerra todos os monitores ativos e sai\n".encode())
     try:
         while True:
             request = client_socket.recv(1024).decode().strip() #para ler até 1024 bytes, decode transforma os bytes recebidos em string e encode transforma a string em bytes, o padrao é utf-8. Strip remove espaços em branco no começo e no final da string
-            if not request:
+            if not request: 
                 break
             #comando para monitor CPU
-            if request.lower().startswith("cpu-"):   #se o comando começa com cpu
+            if request.lower().startswith("cpu-"):   #se o comando começa com cpu-
                 partes = request.split("-") #divide no - em uma lista de dois elementos
                 if len(partes) == 2 and partes[1].isdigit():
                     intervalo = int(partes[1])  #pega o segundo elemento que é o intervalo em segundos
@@ -66,7 +74,7 @@ def handle_client(client_socket, addr):
                 else:
                     client_socket.sendall("Comando inválido. Use cpu-<segundos>\n".encode())
             #comando para monitor Memória
-            elif request.lower().startswith("mem-"): #se o comando começa com mem
+            elif request.lower().startswith("mem-"): #se o comando começa com mem-
                 partes = request.split("-") #divide no - em uma lista de dois elementos
                 if len(partes) == 2 and partes[1].isdigit():
                     intervalo = int(partes[1])  #pega o segundo elemento que é o intervalo em segundos
@@ -89,7 +97,7 @@ def handle_client(client_socket, addr):
                         lista = "Nenhum monitor ativo.\n"
                 client_socket.sendall(f"Monitores ativos:\n{lista}\n".encode())
             #comando para encerrar monitor específico
-            elif request.lower().startswith("quit-"):    #se o comando começa com quit
+            elif request.lower().startswith("quit-"):    #se o comando começa com quit-
                 partes = request.split("-") #divide no - em uma lista de dois elementos
                 if len(partes) == 2 and partes[1].isdigit():
                     mid = int(partes[1])    #pega o segundo elemento que é o id do monitor (mid)
@@ -137,23 +145,16 @@ def start_server():
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #primeiro argumento especifica que estamos usando ip do tipo ipv4 e o segundo que estamos usando o protocolo tcp
         server.bind((server_ip, port))  #vincula o servidor ao ip e porta especificos
         #ouvir conexoes recebidas
-        server.listen(3)    #número máximo de conexões pendentes, esperando para serem aceitas pelo server.accept() 
+        server.listen()    #número máximo de conexões pendentes, esperando para serem aceitas pelo server.accept() 
         print(f"Escutando em:\nIP servidor:{server_ip}\nPorta Servidor:{port}")
         while True:
             #aceitar conexao do cliente
             client_socket, addr = server.accept()   #client socket: cria um novo objeto socket para esse cliente especifico. Addr: o endereco do cliente que se conectou em uma tupla (ip, porta), ip e porta do cliente
-            with lock_clientes:
-                if clientes_ativos >= max_clientes:
-                    client_socket.sendall("Máximo de clientes atingido. Tente novamente mais tarde.\n".encode())
-                    client_socket.close()
-                    continue
-                clientes_ativos += 1
             #criar uma thread para lidar com o cliente
             thread = threading.Thread(target=handle_client, args=(client_socket, addr))
             thread.start()
-            print(f"Conexao aceita de {addr[0]}:{addr[1]}\nClientes ativos: {clientes_ativos}")
-    except Exception as e:
-        print(f"Erro ao iniciar o servidor: {e}")
+    except:
+        print(f"Erro ao iniciar o servidor")
     finally:
         server.close()
 
